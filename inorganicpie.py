667c9a46c4b5cd74d6e32ca2496f66dd2409013b
Noah Proctor <nap2180@columbia.edu>
2026-01-06 13:04:29 -0500

    Update inorganicpie.py
    Signed-off-by: Noah Proctor <nap2180@columbia.edu>

--- inorganicpie.py
+++ inorganicpie.py
@@ -2,16 +2,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import font_manager
 
+# load font
 thin_font_path = '/Users/noahproctor/Library/Fonts/Lato-Thin.ttf'
 bold_font_path = '/Users/noahproctor/Library/Fonts/Lato-Bold.ttf'
 font_manager.fontManager.addfont(thin_font_path)
 font_manager.fontManager.addfont(bold_font_path)
 
-# Set the font properties
+# set the font properties
 thin_prop = font_manager.FontProperties(fname=thin_font_path)
 bold_prop = font_manager.FontProperties(fname=bold_font_path)
 
-#Define Inorganic Data
+# define inorganic data -- taken from spreadsheet 
 copper = 0.1369
 lead = 1.4900
 barium = 0.0160
@@ -21,20 +22,20 @@ nitrate = 1.9000
 data = np.array([copper, lead, barium, fluoride, nitrate])
 total_sum = np.sum(data)
 
-# Calculate percentage for each element
+# calculate percentage for each element
 percentage_copper = (copper / total_sum) * 100
 percentage_lead = (lead / total_sum) * 100
 percentage_barium = (barium / total_sum) * 100
 percentage_fluoride = (fluoride / total_sum) * 100
 percentage_nitrate = (nitrate / total_sum) * 100
 
-# Pie chart configuration
+# pie chart configuration
 labels = ['Copper', 'Lead', 'Barium', 'Fluoride', 'Nitrate']
 sizes = [percentage_copper, percentage_lead, percentage_barium, percentage_fluoride, percentage_nitrate]
 colors = ['#5d8aa8','#367588','#2f4f4f','#264348','#004242']
 explode = (0, 0, 0, 0, 0.025)  
 
-# Pie Chart
+# pie Chart
 y = np.array(sizes)
 
 plt.axis('equal')
